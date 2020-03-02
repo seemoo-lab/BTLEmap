676bf7232e8dfcb74a50c16e7ee70e475ced4df6
Alexander Heinrich <aheinrich@seemoo.tu-darmstadt.de>
2020-03-02 14:36:31 +0100

    Initial Commit

---
+++ BLE-Scanner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BLE-Scanner
+//
+//  Created by Alex - SEEMOO on 02.03.20.
+//  Copyright © 2020 SEEMOO - TU Darmstadt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
